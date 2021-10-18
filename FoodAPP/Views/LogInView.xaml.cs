db21c84be11e1aa1252903df7f44ea7e7faed7dc
danylokaliandra <danylokaliandra@gmail.com>
2021-10-18 14:16:30 -0400

    FoodAPP Initial Commit
    This is my first commit. I created my FoodAPP in Visual Studio, created a Github repository. i created a new content page under the Views folder named it LogInView. Also add images to the drawable folder, added fonts to the helpers folder, then set the LogInView Page as the Main Page.

---
+++ FoodAPP/Views/LogInView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FoodAPP.Views
+{
+    public partial class LogInView : ContentPage
+    {
+        public LogInView()
+        {
+            InitializeComponent();
+        }
+    }
+}
