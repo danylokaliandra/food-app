f25d036b691d7788c0daa3b891f5b874ffb97c37
danylokaliandra <danylokaliandra@gmail.com>
2021-11-12 01:00:11 -0500

    LoginViewModel
    With the ViewModel i created the LoginViewModel to inherit the BaseViewModel, within the constuctor i defined the instance of the LohinCommand and RegisterCommand.

---
+++ FoodAPP/ViewModels/LoginViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading.Tasks;
+using FoodAPP.Services;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FoodAPP.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _Username;
+
+        public string Username
+        {
+            set { this._Username = value;
+                OnPropertyChanged();
+
+            }
+
+            get { return this._Username; }
+        }
+
+        private string _Password;
+
+        public string Password
+        {
+            set
+            {
+                    this._Password = value;
+                    OnPropertyChanged();
+                
+            }
+
+            get { return this._Password; }
+        }
+
+        private bool _IsBusy;
+        public bool IsBusy
+        {
+            set
+            {
+                this._IsBusy = value;
+                OnPropertyChanged();
+            }
+
+            get { return this._IsBusy; }
+        }
+
+        private bool _Result;
+        public bool Result
+        {
+            set
+            {
+                this._Result = value;
+                OnPropertyChanged();
+            }
+
+            get { return this._Result; }
+        }
+
+
+        public Command LoginCommand { get; set; }
+        public Command RegisterCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => await LoginCommandAsyn());
+            RegisterCommand = new Command(async () => await RegisterCommandAsyn());
+        }
+
+        private async Task RegisterCommandAsyn()
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+                var userService = new UserService();
+                Result = await userService.ResgisterUser(Username, Password);
+                if (Result)
+                    await Application.Current.MainPage.DisplayAlert("Success", "User Registered", "OK");
+                else
+                    await Application.Current.MainPage.DisplayAlert("Error", "user already exists with these credentials", "OK");
+
+            }
+
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task LoginCommandAsyn()
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+                var userService = new UserService();
+                Result = await userService.LoginUser(Username, Password);
+                if (Result)
+                {
+                    Preferences.Set("Username", Username);
+                   // await Application.Current.MainPage.Navigation.PopModalAsync(new ProductsView());
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Invalid Username or Password", "OK");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
