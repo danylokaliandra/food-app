11d64ec8738bed6ddfc84d1e9a15f612d2af396d
danylokaliandra <danylokaliandra@gmail.com>
2021-11-23 13:33:01 -0500

    SettingsPage
    Within the views folder, add a contentPage name it SettingsPage, in the SettingsPage.xaml.cs write a method for BuutonCategories_Cicked and a method for ButtonProducts_Clicked on other to add new categories and new products into the firebase. Stacklayout is used

---
+++ FoodAPP/Views/SettingsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FoodAPP.Helpers;
+
+using Xamarin.Forms;
+
+namespace FoodAPP.Views
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+       async void ButtonCategories_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var acd = new AddCategoryData();
+            await acd.AddCategoriesAsync();
+        }
+
+        async void ButtonProducts_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var afd = new AddFoodItemData();
+            await afd.AddFoodItemAsync();
+        }
+    }
+}
