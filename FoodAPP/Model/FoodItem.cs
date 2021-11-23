73da93da34b65b7cb3fb2746b3e447b7162ceee5
danylokaliandra <danylokaliandra@gmail.com>
2021-11-23 13:16:00 -0500

    FoodItem Class
    Within the model folder, add a new file name it FoodItem, define the properties that is required for the food item.

---
+++ FoodAPP/Model/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FoodAPP.Model
+{
+    public class FoodItem
+    {
+       public int ProductID { get; set; }
+       public string ImageUrl { get; set; }
+       public string Name { get; set; }
+       public string Decription { get; set; }
+       public string Rating { get; set; }
+       public string RatingDetail { get; set; }
+       public string HomeSelected { get; set; }
+       public object MyProperty { get; set; }
+       public int CategoryID { get; set; }
+       public int Price { get; set; }
+    }
+}
