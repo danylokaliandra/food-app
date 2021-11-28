e54d0c0439ad7463009189724f465230cf43e4f9
danylokaliandra <danylokaliandra@gmail.com>
2021-11-28 08:09:21 -0500

    CartItemService
    Create a new class within the services folder name it CartItemService that will be able to handle user cart item

---
+++ FoodAPP/Services/CarItemService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FoodAPP.Services
+{
+    public class CarItemService
+    {
+        public int GetUserCartCount()
+        {
+            return 0;
+        }
+
+         public void RemoveItemsFromCart()
+        {
+
+        }
+    }
+}
