14e866f0b7cc733beb75bc21e2ea99286c0af934
danylokaliandra <danylokaliandra@gmail.com>
2021-11-28 08:05:16 -0500

    CategoryDataService
    Create a service folder, with the folder  add a new file name it CategoryDataService, define a method to get list of categories.

---
+++ FoodAPP/Services/CategoryDataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Firebase.Database;
+using FoodAPP.Model;
+using System.Linq;
+using Firebase.Database.Query;
+
+namespace FoodAPP.Services
+{
+    public class CategoryDataService
+    {
+        FirebaseClient client;
+        public CategoryDataService()
+        {
+            client = new FirebaseClient("https://foodapp-9afad-default-rtdb.firebaseio.com/");
+
+        }
+
+        public async Task<List<Category>> GetCategoryAsync()
+        {
+            var categories = (await client.Child("Categories")
+                .OnceAsync<Category>())
+                .Select(c => new Category
+                {
+                    CategoryID = c.Object.CategoryID,
+                    CategoryName = c.Object.CategoryName,
+                    CategoryPoster = c.Object.CategoryPoster,
+                    ImageUrl = c.Object.ImageUrl
+
+                }).ToList();
+            return categories;
+        }
+    }
+}
