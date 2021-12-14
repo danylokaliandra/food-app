d8d5bc2911e4e57bf3e859abc43df3a5ede54819
danylokaliandra <danylokaliandra@gmail.com>
2021-12-14 00:15:43 -0500

    ProductDetailsView
    Within the ViewModel, add a new class and name it ProductsDetailViewModel, within the class inherit the BaseViewModel define the property to maintain the details of the selected food item. Define property to maintain the total quantity selected for foodItem setting the quantity not less than 0 and not greater than 0, define the property for each commands binded to the buttons generate a method to increaseOrder, decreaseOrder,AddToCart

---
+++ FoodAPP/Views/ProductDetailsView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using FoodAPP.Model;
+using FoodAPP.ViewModels;
+using Xamarin.Forms;
+
+namespace FoodAPP.Views
+{
+    public partial class ProductDetailsView : ContentPage
+    {
+        ProductDetailsViewModel pvm;
+        public ProductDetailsView(FoodItem foodItem)
+        {
+            InitializeComponent();
+            pvm = new ProductDetailsViewModel(foodItem);
+            this.BindingContext = pvm;
+        }
+
+       async void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            // await Navigation.PopModalAsync();
+            await Navigation.PopModalAsync();
+
+        }
+    }
+}
