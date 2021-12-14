65f52ada79fe20a0572c35ff4bb644bf9a0bedb2
danylokaliandra <danylokaliandra@gmail.com>
2021-12-14 00:08:03 -0500

    CartItemService
    Within the CartItemService, having two method to count usercartitem and to delete an instance for the SQLite connectiion

--- FoodAPP/Services/CarItemService.cs
+++ FoodAPP/Services/CarItemService.cs
@@ -1,16 +1,27 @@
 ﻿using System;
+using FoodAPP.Model;
+using Xamarin.Forms;
+
 namespace FoodAPP.Services
 {
-    public class CarItemService
+    public class CartItemService
     {
         public int GetUserCartCount()
         {
-            return 0;
+            var cn = DependencyService.Get<ISQLite>().GetConnection();
+            var count = cn.Table<CartItem>().Count();
+            cn.Close();
+            return count;
         }
 
          public void RemoveItemsFromCart()
         {
+            var cn = DependencyService.Get<ISQLite>().GetConnection();
+            cn.DeleteAll<CartItem>();
+            cn.Commit();
+            cn.Close();
 
         }
     }
 }
+ 
