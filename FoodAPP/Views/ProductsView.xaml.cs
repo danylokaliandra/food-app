522ecf92204ff39ccdd905c171cbbbf797d462f7
danylokaliandra <danylokaliandra@gmail.com>
2021-12-14 00:21:12 -0500

    LogOutView
    Within the View, add a new class and name it Logout, set the binding context, set the label, text and pancake view using the stackLayout, also add a productView, ordersView, productsViewModel

---
+++ FoodAPP/Views/ProductsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+using FoodAPP.Model;
+
+
+namespace FoodAPP.Views
+{
+    public partial class ProductsView : ContentPage
+    {
+        public ProductsView()
+        {
+            InitializeComponent();
+        }
+
+       async void CollectionView_SelectionChanged(System.Object sender, Xamarin.Forms.SelectionChangedEventArgs e)
+        {
+            var category = e.CurrentSelection.FirstOrDefault() as Category;
+            if (category == null)
+                return;
+
+            await Navigation.PushModalAsync(new CategoryView(category));
+
+            ((CollectionView) sender).SelectedItem = null;
+        }
+    }
+}
